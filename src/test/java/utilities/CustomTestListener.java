a217959d7a9fd0a3bfb6c4418bbeb4414435f5b0
Mustafa Emre <m.emreatci@gmail.com>
2024-05-02 13:05:15 +0300

    5/2

--- src/test/java/utilities/CustomTestListener.java
+++ src/test/java/utilities/CustomTestListener.java
@@ -10,28 +10,24 @@ public class CustomTestListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
         ITestListener.super.onTestStart(result);
-        System.out.println("Test başladı: " + result.getName());
-        log.info("Test başladı: " + result.getName());
+        log.info("Test started : " + result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         ITestListener.super.onTestSuccess(result);
-        System.out.println("Test başarılı: " + result.getName());
-        log.info("Test başarılı: " + result.getName());
+        log.info("Test successful: " + result.getName());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         ITestListener.super.onTestFailure(result);
-        System.out.println("Test başarısız: " + result.getName());
-        log.info("Test başarısız: " + result.getName());
+        log.error("Test unsuccessful: " + result.getName());
     }
 
     @Override
     public void onFinish(ITestContext context) {
         ITestListener.super.onFinish(context);
-        System.out.println("Test tamamlandı: " + context.getName());
-        log.info("Test tamamlandı: " + context.getName());
+        log.info("Test finished : " + context.getName());
     }
 }
